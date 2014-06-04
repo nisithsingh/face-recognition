ffff76f33509ecb87403f71fffe8b6878932d58d
Saurav Verma <viczsaurav@gmail.com>
2014-06-04 23:08:02 +0800

    Timer Enabled Upload

--- facerecog/static/js/webcam.js
+++ facerecog/static/js/webcam.js
@@ -1,90 +1,70 @@
-var webcam = (function(){
-
-    var video = document.getElementById("live_video"),
-        canvas = document.getElementById('canvas');
-
-    function play() {
-        if (navigator.getUserMedia) {
-            navigator.getUserMedia({video: true, audio: true, 
-              toString : function() {return "video,audio";} }, onSuccess, onError);
-        } else {
-            changeStatus('getUserMedia is not supported in this browser.', true);
-        }
-    }
-
-    function onSuccess(stream) {
-        var source;
-         if (window.webkitURL) {
-            source = window.webkitURL.createObjectURL(stream);
-        } else {
-            source = stream; // Opera and Firefox
-        }
+    var person;
+    var timer = 0;
+    var imgCount = 1;
+    
+    window.addEventListener("DOMContentLoaded", function () {
+    // Grab elements, create settings, etc.
+   
+     var   video = document.getElementById("video"),
+        videoObj = { "video": true },
+        errBack = function (error) {
+            console.log("Video capture error: ", error.code);
+        };
  
-        video.autoplay = true;
-        video.src = source;
-        changeStatus('Connected.', false);
+    if (navigator.getUserMedia) { // Standard
+        navigator.getUserMedia(videoObj, function (stream) {
+            video.src = stream;
+            video.play();
+        }, errBack);
+    } else if (navigator.webkitGetUserMedia) { // WebKit-prefixed
+        navigator.webkitGetUserMedia(videoObj, function (stream) {
+            video.src = window.webkitURL.createObjectURL(stream);
+            video.play();
+        }, errBack);
     }
-
-    function onError() {
-        changeStatus('Please accept the getUserMedia permissions! Refresh to try again.', true);
-    }
-
-    function changeStatus(msg, error) {
-        var status = document.getElementById('status');
-        status.innerHTML = msg;
-        status.style.color = (error) ? 'red' : 'green';
-    }
-
-
-    // allow the user to take a screenshot
-    function setupPhotoBooth() {
-        var takeButton = document.getElementById("Submit");
-        takeButton.addEventListener('click', takePhoto, true);
+ 
+    // Trigger photo take
+    document.getElementById("snap").addEventListener("click", function () {
+        if (person == null) {
+          person = prompt("Please enter your name");
+        }
+        timer = setInterval(function(){UploadToServer()},1000);
         
+    });
+}, false);
 
-        // var saveButton = document.createElement('button');
-        // saveButton.id = 'save';
-        // saveButton.innerText = 'Save!';
-        // saveButton.disabled = true;
-        // saveButton.addEventListener('click', savePhoto, true);
-        // document.body.appendChild(saveButton);
 
+function UploadToServer() {
+
+    var canvas = document.getElementById("canvas"),
+        context = canvas.getContext("2d");
+    context.drawImage(video, 0, 0, 600, 400);
+
+    var img = canvas.toDataURL('image/jpeg', 0.9).split(',')[1];
+
+
+    $.ajax({
+        url: "/save/",
+        type: "POST",
+        data: "{ 'person': '" + person +"','image': '" + img + "' }",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function () {
+            //alert('Image Uploaded!!');
+            $("#snap").removeAttr('disabled');
+            $("#snap").attr("value", "Uploaded image " + imgCount + " of 10" );
+            // $("#snap").attr("value", "Upload");
+        },
+        error: function (e) {
+            alert("Error : " + e);
+            $("#snap").removeAttr('disabled');
+            $("#snap").attr("value", "Upload");
+        }
+    });
+
+    if(imgCount == 10) {
+        clearInterval(timer);
+      } else {
+        imgCount++;
+      }
     }
-
-    function takePhoto() {
-
-        // set our canvas to the same size as our video
-        canvas.width = video.width;
-        canvas.height = video.height;
-
-        var context = canvas.getContext('2d');
-        context.drawImage(video, 0, 0, canvas.width, canvas.height);
-        
-        // allow us to save
-        // var saveButton = document.getElementById('save');
-        // saveButton.disabled = false;
-
-    }
-
-    function savePhoto() {
-        var data = canvas.toDataURL("image/png");
-        data = data.replace("image/png","image/octet-stream");
-        document.location.href = data;
-
-    }
-
-    return {
-        init: function() {
-            changeStatus('Please accept the permissions dialog.', true);
-            video.width = 640;
-            video.height = 480;
-            document.body.appendChild(video);
-            document.body.appendChild(canvas);
-            navigator.getUserMedia || (navigator.getUserMedia = navigator.mozGetUserMedia || navigator.webkitGetUserMedia || navigator.msGetUserMedia);
-            play();
-            setupPhotoBooth();
-        }()
-
-    }
-
-})();
